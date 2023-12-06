194708742f3a3553867e1539ea7adc35744245b8
“Lukabaramishvili” <lukabaramishvili@gmail.com>
2023-12-06 10:29:20 +0100

    second commit

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <section className="py-24"> 
       <div className="container">
-        <h1 className="text-3xl font-bold mt-4">NextJs Starter Kit</h1>
+        <h1 className="text-3xl font-bold mt-4">NextJs Starter Kit 1</h1>
       </div>
     </section>
   )
