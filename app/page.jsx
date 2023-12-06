fe03336ed98a361be526e68a7593a99d4471e9a5
“Lukabaramishvili” <lukabaramishvili@gmail.com>
2023-12-06 10:25:32 +0100

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className="py-24"> 
+      <div className="container">
+        <h1 className="text-3xl font-bold mt-4">NextJs Starter Kit</h1>
+      </div>
+    </section>
+  )
+}
